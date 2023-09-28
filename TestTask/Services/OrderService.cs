11cd72fa2139c0a067a8421faa4015365bd9103b
DmitriyLeonov <dleonov701@gmail.com>
2023-09-28 15:00:28 +0300

    Add project files.

---
+++ TestTask/Services/OrderService.cs
@@ -0,0 +1,24 @@
+﻿using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services
+{
+    public class OrderService : IOrderService
+    {
+        private readonly ApplicationDbContext _db;
+
+        public OrderService(ApplicationDbContext db) {
+            _db = db;
+        }
+        public async Task<Order> GetOrder()
+        {
+            return _db.Orders.OrderByDescending(o => o.Quantity * o.Price).FirstOrDefault();
+        }
+
+        public async Task<List<Order>> GetOrders()
+        {
+            return _db.Orders.Where(o => o.Quantity > 10).ToList();
+        }
+    }
+}
---
+++ TestTask/Services/UserService.cs
@@ -0,0 +1,24 @@
+﻿using TestTask.Data;
+using TestTask.Enums;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly ApplicationDbContext _db;
+        public UserService(ApplicationDbContext db) {
+            _db = db;
+        }
+        public async Task<User> GetUser()
+        {
+            return _db.Users.OrderByDescending(u => u.Orders.Count).FirstOrDefault();
+        }
+
+        public async Task<List<User>> GetUsers()
+        {
+            return _db.Users.Where(u => u.Status.Equals(UserStatus.Inactive)).ToList();
+        }
+    }
+}
